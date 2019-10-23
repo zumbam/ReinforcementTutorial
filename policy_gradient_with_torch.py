3d2c3e67cf2a66c5804a6ec8e8080089a5a2eafd
stefan ronczka <ronczka@hm.edu>
2019-10-23 16:28:29 +0200

    Adding some commends.

--- policy_gradient_with_torch.py
+++ policy_gradient_with_torch.py
@@ -6,10 +6,13 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 
-env = gym.make('CartPole-v1')
+
 
 
 class PolicyNet(torch.nn.Module):
+    """
+    This is the policy network. It estimates a distribution for choosing a appropriate action for a given state.
+    """
 
     def __init__(self):
         super(PolicyNet, self).__init__()
@@ -30,13 +33,21 @@ class PolicyNet(torch.nn.Module):
         return net
 
 
-state = env.reset()
-num_observations = len(state)
-policy_net = PolicyNet()
+
 
 def sample_trajectories(model, num_samples, max_num_sequence_elems=10):
+    '''
+    "On Policy sampling" for preparing trajectories for a given model.
+
+    :param model: The policy model
+    :param num_samples: the batchsize (or number of samples for monte carlo estimation)
+    :param max_num_sequence_elems: the maximum number of elements till game is done (default in cartpole is 200)
+    :return: list for all sample trajectories containing [probabilities for each action decision, given rewards,
+     the action taken
+    '''
     softmax = torch.nn.Softmax()
 
+    # sampling random numbers for all possible states and samples.
     sampling = np.random.rand(num_samples, max_num_sequence_elems)
 
     actions_list = []
@@ -52,14 +63,14 @@ def sample_trajectories(model, num_samples, max_num_sequence_elems=10):
         observations = []
         for j in range(max_num_sequence_elems):
 
+            # evaluating the policy network for action probabilities.
             observation_tensor = torch.tensor(np.expand_dims(state, 0), dtype=torch.float32)
             res = model(observation_tensor)
 
             prob = softmax(res)[0]
-
-
             action = 0
             reward = 0
+            # making discrete action sampling to possible explore the whole sampling space.
             if sampling[i, j] > prob[0]:
                 action = 1
             else:
@@ -83,25 +94,21 @@ def sample_trajectories(model, num_samples, max_num_sequence_elems=10):
     return probs_list, rewards_list, actions_list
 
 
-
-# def gradient_loss_gradient(model_, probs_, rewards_):
-#     decaying_discount_factor = torch.tensor(np.linspace(1, 0.1, max_num_sequence_elems))
-#     losses = []
-#     for b in range(len(probs_)):
-#         rewards = rewards_[b]
-#         probs = probs_[b]
-#         discounted_reward_args = [decaying_discount_factor[i] * rewards[i] for i in range(len(rewards))]
-#         G = torch.sum(torch.stack(discounted_reward_args))
-#         log_probs_args = [torch.log(probs[i]) for i in range(len(probs))]
-#         log_probs = torch.sum(torch.stack(log_probs_args))
-#         losses.append(log_probs * G)
-#
-#     loss_tensor = torch.stack(losses)
-#     loss = torch.mean(loss_tensor)
-#     return loss
-
 def maximum_expected_reward_loss(model_, probs_, rewards_, actions_, loss_):
+    '''
+    This is function returns the policy loss function resulting in the policy gradient. Interestingly the cross
+    entropy function loss has exactly the need attributes for the policy gradient "1/n * sum_b_B(sum_t_T (grad (log (pi(a|s)))) *G_T)".
+    The cross entropy results exactly in grad (log (pi(a|s)).
 
+    :param model_: the policy model
+    :param probs_: the probabilities.: tensor [[a, b], [a, b]]
+    :param rewards_: the rewards collected a trajectory.: tensor [r1, r2, ..., rn]
+    :param actions_: the actions taken in the samples.: tensor [a1, a2, ..., an]
+    :param loss_: the loss function to use. (avoid recreation of graph nodes).: torch.nn.loss
+    :return: the mean maximum likelihood loss over all samples.: tensor
+    '''
+
+    # this is the used discount function decaying till for giving long time goals less attention.
     decaying_discount_factor = torch.tensor(np.linspace(1, 0.1, max_num_sequence_elems))
     losses = []
 
@@ -122,6 +129,11 @@ def maximum_expected_reward_loss(model_, probs_, rewards_, actions_, loss_):
     return loss
 
 def measure_expected_reward(rewards_):
+    '''
+    This function evaluates the current mean expectation.
+    :param rewards_: the reward from a sampled trajectory
+    :return: the mean reward over all given trajectories.:scalar
+    '''
     reward_sums = []
     for b in range(len(rewards_)):
         rewards = np.array(rewards_[b])
@@ -130,24 +142,28 @@ def measure_expected_reward(rewards_):
     mean_reward = np.mean(np.array(reward_sums))
     return mean_reward
 
-num_episodes = 10000
+# num of experiment to run
+num_episodes = 3000
+# num of samples to use for monte carlo estimation
 batch_size = 32
+# maximum number of states till game is done
 max_num_sequence_elems = 200
 
-optimizer = torch.optim.SGD(policy_net.parameters(), lr=0.01)
+# init playground
+env = gym.make('CartPole-v1')
+# init network
+policy_net = PolicyNet()
 
+# init optimization needs
+optimizer = torch.optim.SGD(policy_net.parameters(), lr=0.01)
 expected_rewards = []
 steps = []
 entropy = torch.nn.CrossEntropyLoss()
 for i in range(num_episodes):
 
     probs, rewards, actions = sample_trajectories(policy_net, batch_size, max_num_sequence_elems=max_num_sequence_elems)
-
-    # loss = gradient_loss_gradient(policy_net, probs, rewards)
     loss = maximum_expected_reward_loss(policy_net, probs, rewards, actions, entropy)
-    # grads = []
-    # for l in loss:
-    #
+
     optimizer.zero_grad()
     loss.backward()
 
@@ -165,7 +181,7 @@ for i in range(num_episodes):
         d = {'steps': steps, 'expected_reward': expected_rewards}
         df = pd.DataFrame(data=d)
         df.to_csv(f'expected_reward{i}.csv')
-        torch.save(policy_net.state_dict(), f'mynet{i}.pt')
+        torch.save(policy_net.state_dict(), f'mynet_{i}.pt')
 
 d = {'steps': steps, 'expected_reward': expected_rewards}
 df = pd.DataFrame(data=d)
@@ -175,8 +191,7 @@ df.to_csv('expected_reward.csv')
 sns.set()
 sns.relplot(x='steps', y='expected_reward', data=df, kind='line')
 
-torch.save(policy_net.state_dict(), 'mynet.pt')
+torch.save(policy_net.state_dict(), 'mynet_last.pt')
 plt.show()
 
-torch.save(policy_net.state_dict(), 'mynet_2.pt')
 print(f'reach episode {num_episodes}')
